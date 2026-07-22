96a2a2205066e2f2ca6c96507ea3d9c1f40640f1
zzz <cuelguerandom@gmail.com>
2026-07-22 09:29:10 -0300

    se agregaron las estructuras reg_t,cpu_t y la funcion fetch_inst

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
+#include <stdint.h>
 
+typedef struct {
+   uint16_t pc; 
+   /* Puede que sea mejor un array pero voy a dejarlo separado con sus nombres */
+   uint8_t ac;
+   uint8_t x;
+   uint8_t y;
+   /* Status register 
+
+    - : significa que ese bit se ignora.
+    Negative | Overflow | - | Break | Decimal (use BCD for arimetic) | Interrump | Zero | Carry */
+   uint8_t sr; 
+   uint8_t sp;
+} reg_t;
+
+typedef struct {
+    uint8_t inst;
+    reg_t reg;
+} cpu_t;
+
+void fetch_inst(uint8_t *ram,uint16_t *pc,uint8_t *inst)
+{
+   *inst = ram[*pc];
+   (*pc)++;
+}
 
 int main()
 {
