d5f20e91331ad5db35a99a5487e858927531e07b
zzz <cuelguerandom@gmail.com>
2026-07-28 10:44:50 -0300

    se agregaron las funciones sbc, sbc_overf y cmpIndex ademas dea el enum reset_t

--- main.c
+++ main.c
@@ -2,16 +2,28 @@
 #include <stdint.h>
 
 typedef enum {
-    M_NULO = 0, //agregado solo para la funcion Overflow 
-    M_CARRY = 1,
-    M_ZERO,
-    M_INTER = 4,
-    M_DEC = 8,
-    M_BREAK = 16,
-    M_IGNOR = 32,
-    M_OVERF = 64,
-    M_NEG = 128,
-} mask_t;
+    OR_NULO = 0, //agregado solo para la funcion Overflow 
+    OR_CARRY = 1,
+    OR_ZERO,
+    OR_INTER = 4,
+    OR_DEC = 8,
+    OR_BREAK = 16,
+    OR_IGNOR = 32,
+    OR_OVERF = 64,
+    OR_NEG = 128,
+} or_mask_t;
+
+
+typedef enum {
+    RESET_NEG = 127,
+    RESET_OVERF = 191,
+    RESET_BREAK = 239,
+    RESET_DEC = 247,
+    RESET_INTER = 251,
+    RESET_ZERO = 253,
+    RESET_CARRY = 255,
+} resetF_t;
+
 
 typedef struct {
     uint16_t pc; 
@@ -50,7 +62,7 @@ void brk(cpu_t *cpu, uint8_t *ram)
     ram[cpu->reg.sp] = (cpu->reg.pc & 0xFF00) >> 8;
     cpu->reg.sp--;
     ram[cpu->reg.sp] = (cpu->reg.pc & 0xFF);
-    cpu->reg.sr &= M_INTER;
+    cpu->reg.sr &= OR_INTER;
     cpu->reg.sp--;
     ram[cpu->reg.sp] = cpu->reg.sr;
     cpu->reg.pc = (ram[0xFFFF] << 8) | ram[0xFFFE];
@@ -65,22 +77,22 @@ void brk(cpu_t *cpu, uint8_t *ram)
 
 /* Una manera muy torpe, pero deberia resolver.
      al final resolver es lo primero, mejorar es lo siguiente.*/
-mask_t overflow(uint8_t ac,uint8_t value, uint8_t carry)
+or_mask_t overflow(uint8_t ac,uint8_t value, uint8_t carry)
 {
     
-    mask_t resul = M_NULO;
+    or_mask_t resul = OR_NULO;
     uint8_t sum = ac + value;
     /*Acá lo importante es que para la suma tengo Overflow
      si al sumar dos números negativos obtengo uno positivo
     y si sumo dos números positivos obtengo uno negativo.*/
     if (ac <= 127 && value <= 127) {
         if (sum >= 128) {
-            resul = M_OVERF;
-        } else if (sum + carry == 128) resul = M_OVERF;
+            resul = OR_OVERF;
+        } else if (sum + carry == 128) resul = OR_OVERF;
     } else if (ac >= 128 && value >= 128) {
                 if (sum <= 127) {
-                    resul = M_OVERF;
-                } else if (sum + carry <= 127) resul = M_OVERF;
+                    resul = OR_OVERF;
+                } else if (sum + carry <= 127) resul = OR_OVERF;
     }
     
     return resul;
@@ -88,25 +100,25 @@ mask_t overflow(uint8_t ac,uint8_t value, uint8_t carry)
 
 /* En caso de que el valor no sea cero
    el valor que retorna es cero. Si value
-   es cero retor M_ZERO que es igual a 2
+   es cero retor OR_ZERO que es igual a 2
    este valor sirve como mascara para el registro
    de estado */
-mask_t zero(uint8_t value)
+or_mask_t zero(uint8_t value)
 {
-    mask_t resul = M_NULO;
-    if ((value & 0xFF) == 0) resul = M_ZERO;
+    or_mask_t resul = OR_NULO;
+    if ((value & 0xFF) == 0) resul = OR_ZERO;
     return resul;
 }
 
 void adc(uint8_t *ac, uint8_t value, uint8_t *sr)
 {  
-    uint16_t resul = *ac + value + (*sr & M_CARRY);
+    uint16_t resul = *ac + value + (*sr & OR_CARRY);
     uint8_t carry = (resul & 0x100) >> 8;
     *sr = *sr & 0xFE | carry; 
  
     (*sr) |= zero(resul);
  
-    *sr &= (resul & M_NEG);
+    *sr &= (resul & OR_NEG);
     //TODO: falta setear el flag de overflow.
     *sr |= overflow(*ac,value,carry);
     
@@ -119,11 +131,53 @@ void cmp(uint8_t ac,uint8_t value,uint8_t *sr)
     uint8_t resul = ac - value;
     (*sr) &= 0xFD;
     (*sr) |= zero(resul);
-    (*sr) &= (M_NEG - 1);
-    (*sr) |= resul & M_NEG;
-    if (resul >= 0) (*sr) |= M_CARRY;
+    (*sr) &= (OR_NEG - 1);
+    (*sr) |= resul & OR_NEG;
+    if (resul >= 0) (*sr) |= OR_CARRY;
     else (*sr) &= 0xFE;
 }
+
+
+/* A diferencia dea cpm esta instruccion
+   trabaja con los registros X e Y ademas
+   usa una direccion de memoria para acceder
+   a un valor po lo demas se comporta como CMP*/
+void cpmIndex(uint8_t reg,uint8_t *sr,uint8_t ram[],uint16_t dir)
+{
+    uint8_t value = ram[dir];
+    cmp(reg,value,sr);
+}
+
+
+/* Overflow en caso de la resta, si mis operandos
+   tienen distinto signo y el resultado tiene distinto
+   signo que el acumulador tengo Overflow
+   como zero o overflow este devuelve OR_ZERO en caso
+   de que no haya overflow y OR_OVERF en caso de que lao haya*/
+or_mask_t sbc_overf(uint8_t sR,uint8_t sA,uint8_t sMC)
+{
+    or_mask_t result = OR_ZERO;
+    if (sA != sMC && sA != sR) result = OR_OVERF;
+    return result;
+}
+
+void sbc(uint8_t *ac,uint8_t value,uint8_t *sr)
+{
+    /* Salvo que este pasando por alto algún capricho de C
+     esde deberia permitirme quedarme con el opuesto del carry */
+    uint8_t compl = ~(*sr & OR_CARRY);
+    if (*ac >= (value + compl)) (*sr) |= OR_CARRY;
+    else (*sr) &= 0xFE;
+    uint16_t resul = *ac - value - compl;
+    (*sr) &= 0xFD;
+    (*sr) |= zero(resul);
+    (*sr) &= 0x7F;
+    (*sr) |= (resul & OR_NEG);
+    (*sr) &= RESET_OVERF;
+    
+    
+}
+
 
 int main()
 {
